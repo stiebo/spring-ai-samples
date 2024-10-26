53aa7cfa45438914d9d962f713bf42dc371aafc9
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-10-26 16:47:39 +0200

    Initial commit

---
+++ src/main/java/dev/stiebo/openaiutils/OpenaiutilsApplication.java
@@ -0,0 +1,13 @@
+package dev.stiebo.openaiutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiutilsApplication.class, args);
+	}
+
+}
