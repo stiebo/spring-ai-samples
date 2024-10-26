d5b74863d287d0970f92def6cc200abce00a82ff
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-10-26 16:51:56 +0200

    Initial setup

--- src/main/java/dev/stiebo/openaiutils/OpenAiutilsApplication.java
+++ src/main/java/dev/stiebo/openaiutils/OpenAiutilsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenaiutilsApplication {
+public class OpenAiutilsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpenaiutilsApplication.class, args);
+		SpringApplication.run(OpenAiutilsApplication.class, args);
 	}
 
 }
