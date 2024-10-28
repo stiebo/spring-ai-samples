73562737aa0f2e688f9ad9681f1f557dc0687cc8
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-10-28 16:15:27 +0100

    add NotEmptyFile constraint

--- src/main/java/dev/stiebo/openaiutils/controller/OpenAiUtilsController.java
+++ src/main/java/dev/stiebo/openaiutils/controller/OpenAiUtilsController.java
@@ -2,9 +2,9 @@ package dev.stiebo.openaiutils.controller;
 
 import dev.stiebo.openaiutils.dto.CVDataOutDto;
 import dev.stiebo.openaiutils.dto.Flashcard;
-import dev.stiebo.openaiutils.exception.FileErrorException;
 import dev.stiebo.openaiutils.service.CVService;
 import dev.stiebo.openaiutils.service.FlashcardService;
+import dev.stiebo.openaiutils.validation.NotEmptyFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/openaiutils")
@@ -28,28 +29,30 @@ public class OpenAiUtilsController {
         this.flashcardService = flashcardService;
     }
 
+    private void confirmFileExistsOrThrowException (MultipartFile file) {
+
+    }
+
     @PostMapping("/analyzeCV")
-    public CVDataOutDto analyzeCv(@RequestParam(value = "file", required = false) MultipartFile file) {
-        // TODO could move this to custom @NotEmptyFile annotation
-        if (file == null || file.isEmpty()) {
-            throw new FileErrorException("Uploaded file is empty. Please upload a valid file.");
-        }
+    public CVDataOutDto analyzeCv(@RequestParam(value = "file", required = false)
+                                      @NotEmptyFile MultipartFile file) {
         return cvService.getCVData(file);
     }
 
     @PostMapping("/createCsvFlashcards")
-    public byte[] createCSvFlashcards (@RequestParam("file") MultipartFile file) {
-        if (file == null || file.isEmpty()) {
-            throw new FileErrorException("Uploaded file is empty. Please upload a valid file.");
-        }
+    public byte[] createCSvFlashcards (@RequestParam("file") @NotEmptyFile MultipartFile file) {
         return flashcardService.createCsvFlashcardsFromFile(file);
     }
 
     @PostMapping("/createFlashcards")
-    public List<Flashcard> createFlashcards (@RequestParam("file") MultipartFile file) {
-        if (file == null || file.isEmpty()) {
-            throw new FileErrorException("Uploaded file is empty. Please upload a valid file.");
-        }
+    public List<Flashcard> createFlashcards (@RequestParam(value = "file", required = false)
+                                                 @NotEmptyFile MultipartFile file) {
         return flashcardService.createFlashcardsFromFile(file);
+    }
+
+    @PostMapping("/chatwithmydocs/adddoc")
+    public Map<String,String> addDocToRepository (@RequestParam(value = "file", required = false)
+                                                      @NotEmptyFile MultipartFile file) {
+        return Map.of("response", "File has been added to your document store.");
     }
 }
