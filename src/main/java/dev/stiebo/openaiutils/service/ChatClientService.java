790c5b3a1c941059b28a459af47d6b41695f7b16
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-10-31 19:32:08 +0100

    remove unused interface method

--- src/main/java/dev/stiebo/openaiutils/service/ChatClientService.java
+++ src/main/java/dev/stiebo/openaiutils/service/ChatClientService.java
@@ -3,7 +3,6 @@ package dev.stiebo.openaiutils.service;
 import org.springframework.core.io.Resource;
 
 public interface ChatClientService {
-    <T> T getResponse(Class<T> responseType, Resource userPromptResource);
     <T> T getResponse(Class<T> responseType, Resource userPromptResource, Resource imageResource);
     <T> T getResponse(Class<T> responseType, Resource userPromptResource, String document);
 }
