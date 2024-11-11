e3ba2191a73be9e73c65736d7f22ef7d23664ce7
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-11-11 09:16:07 +0100

    use ErrorResponse classes instead of Map<>

---
+++ src/main/java/dev/stiebo/aiutilities/exception/ValidationErrorResponse.java
@@ -0,0 +1,19 @@
+package dev.stiebo.aiutilities.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ValidationErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        List<FieldError> fieldErrors,
+        String path
+) {
+    public record FieldError(
+            String fieldName,
+            String errorMessage
+    ){
+
+    }
+}
