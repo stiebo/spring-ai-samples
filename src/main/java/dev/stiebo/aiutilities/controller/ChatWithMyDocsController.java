b2a0187261427afac08f60d4340373efea9ba332
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-11-14 09:29:11 +0100

    split controller into multiple

--- src/main/java/dev/stiebo/aiutilities/controller/ChatWithMyDocsController.java
+++ src/main/java/dev/stiebo/aiutilities/controller/ChatWithMyDocsController.java
@@ -1,16 +1,11 @@
 package dev.stiebo.aiutilities.controller;
 
-import dev.stiebo.aiutilities.dto.CVDataOutDto;
 import dev.stiebo.aiutilities.dto.ChatInDto;
-import dev.stiebo.aiutilities.dto.Flashcard;
 import dev.stiebo.aiutilities.dto.DocsOutDto;
 import dev.stiebo.aiutilities.exception.ErrorResponse;
 import dev.stiebo.aiutilities.exception.ValidationErrorResponse;
-import dev.stiebo.aiutilities.service.CVService;
 import dev.stiebo.aiutilities.service.ChatWithMyDocsService;
-import dev.stiebo.aiutilities.service.FlashcardService;
 import dev.stiebo.aiutilities.validation.NotEmptyFile;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -32,69 +27,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 @Validated
-public class AiUtilitiesController {
-    private final CVService cvService;
-    private final FlashcardService flashcardService;
+public class ChatWithMyDocsController {
     private final ChatWithMyDocsService chatWithMyDocsService;
 
     @Autowired
-    public AiUtilitiesController(CVService cvService, FlashcardService flashcardService,
-                                 ChatWithMyDocsService chatWithMyDocsService) {
-        this.cvService = cvService;
-        this.flashcardService = flashcardService;
+    public ChatWithMyDocsController(ChatWithMyDocsService chatWithMyDocsService) {
         this.chatWithMyDocsService = chatWithMyDocsService;
-    }
-
-    @Operation(summary = "Analyze CV",
-            description = "Analyze any CV and get a breakdown of its content in json")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "CV analyzed",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = CVDataOutDto.class))),
-            @ApiResponse(responseCode = "400", description = "Validation failed for request parameter",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ValidationErrorResponse.class)
-                    )),
-            @ApiResponse(responseCode = "422", description = "File Error",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ErrorResponse.class)
-                    ))
-    })
-    @PostMapping(value = "/analyzeCV", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public CVDataOutDto analyzeCv(@RequestParam(value = "file", required = false)
-                                  @NotEmptyFile MultipartFile file) {
-        return cvService.getCVData(file);
-    }
-
-    @Hidden
-    @PostMapping(value = "/createCsvFlashcards", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public byte[] createCSvFlashcards(@RequestParam("file") @NotEmptyFile MultipartFile file) {
-        return flashcardService.createCsvFlashcardsFromFile(file);
-    }
-
-    @Operation(summary = "Create Flashcards",
-            description = "Generates study flashcards from text (pdf) or image files with AI")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Flashcards generated",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            array = @ArraySchema(schema = @Schema(implementation = Flashcard.class)))),
-            @ApiResponse(responseCode = "400", description = "Validation failed for request parameter",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ValidationErrorResponse.class)
-                    )),
-            @ApiResponse(responseCode = "422", description = "File Error",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ErrorResponse.class)
-                    ))
-    })
-    @PostMapping(value = "/createFlashcards", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public List<Flashcard> createFlashcards(@RequestParam(value = "file", required = false)
-                                            @NotEmptyFile MultipartFile file) {
-        return flashcardService.createFlashcardsFromFile(file);
     }
 
     @Operation(summary = "Add Document to Repository",
