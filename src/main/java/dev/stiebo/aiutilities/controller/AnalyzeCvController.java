d6b71a7834d2648a97b700d33a6b41711b1f2b0c
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-11-15 11:59:42 +0100

    remove unused import statement

--- src/main/java/dev/stiebo/aiutilities/controller/AnalyzeCvController.java
+++ src/main/java/dev/stiebo/aiutilities/controller/AnalyzeCvController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/api")
 @Validated
--- src/main/java/dev/stiebo/aiutilities/controller/ChatWithMyDocsController.java
+++ src/main/java/dev/stiebo/aiutilities/controller/ChatWithMyDocsController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
